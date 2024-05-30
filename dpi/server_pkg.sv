5537eb86d12a116ef6304559875bd2e2bb8536a5
mahmoud amiri <itsmahmoudamiri@gmail.com>
2024-05-30 14:41:03 +0200

    server example has been tested

--- dpi/server_pkg.sv
+++ dpi/server_pkg.sv
@@ -1,12 +1,12 @@
 package server_pkg;
 
-    import "DPI-C" function void start_server(input int port);
-    import "DPI-C" function void stop_server();
-    import "DPI-C" function void send_data(input string data);
-    import "DPI-C" function void receive_data(output string data);
-    import "DPI-C" function int handshake();
-    import "DPI-C" function void send_large_data(input string data);
-    import "DPI-C" function string receive_large_data(input int max_attempts);
+    import "DPI-C" function void sv_start_server(input int port);
+    import "DPI-C" function void sv_stop_server();
+    import "DPI-C" function int sv_handshake();
+    import "DPI-C" function void sv_send_large_data(input string json_str);
+    import "DPI-C" function string sv_receive_large_data(input int max_attempts);
+    import "DPI-C" function void sv_send_data(input string json_str);
+    import "DPI-C" function void sv_receive_data(input string json_str);
 
     class Server;
         int port;
@@ -17,21 +17,27 @@ package server_pkg;
         endfunction
 
         task start();
-            start_server(port);
+            sv_start_server(port);
             $display("Server started on port %0d", port);
+            if (sv_handshake()) begin
+                $display("Handshake successful");
+            end else begin
+                $display("Handshake failed");
+                sv_stop_server();
+            end
         endtask
 
         task stop();
-            stop_server();
+            sv_stop_server();
             $display("Server stopped on port %0d", port);
         endtask
 
         task send(string data);
-            send_data(data);
+            sv_send_data(data);
         endtask
 
         task receive(output string data);
-            receive_data(data);
+            sv_receive_data(data);
             this.received_data = data;
         endtask
 
@@ -39,23 +45,13 @@ package server_pkg;
             return received_data;
         endfunction
 
-        task automatic perform_handshake();
-            int result;
-            result = handshake();
-            if (result) begin
-                $display("Handshake successful");
-            end else begin
-                $display("Handshake failed");
-                stop();
-            end
-        endtask
 
         task send_large(string data);
-            send_large_data(data);
+            sv_send_large_data(data);
         endtask
 
         function string receive_large(int max_attempts);
-            return receive_large_data(max_attempts);
+            return sv_receive_large_data(max_attempts);
         endfunction
     endclass
 
--- server_example/server_sv.sv
+++ server_example/server_sv.sv
@@ -1,36 +1,23 @@
+import server_pkg::*;
+
 module top;
 
-    import "DPI-C" function void sv_start_server(input int port);
-    import "DPI-C" function void sv_stop_server();
-    import "DPI-C" function int sv_handshake();
-    import "DPI-C" function void sv_send_large_data(input string json_str);
-    import "DPI-C" function string sv_receive_large_data(input int max_attempts);
+    Server srv;
+    string received_data;
+    string data_to_send;
+    int port;
 
     initial begin
-        int port = 8080;
-        string received_data;
-        string data_to_send;
-        // Start the server
-        sv_start_server(port);
-        
-        // Perform handshake
-        if (sv_handshake()) begin
-            $display("Handshake successful");
+        port = 8080;
+        srv = new(port);
+        srv.start();
+        received_data = srv.receive_large(10);
+        $display("Received large data: %s", received_data);
 
-            // Example to receive large data
-            received_data = sv_receive_large_data(10);
-            $display("Received large data: %s", received_data);
+        data_to_send = "{\"key\": \"value\"}";
+        srv.send_large(data_to_send);
 
-            // Example data to send
-            
-            data_to_send = "{\"key\": \"value\"}";
-            sv_send_large_data(data_to_send);
-        end else begin
-            $display("Handshake failed");
-        end
-
-        // Stop the server
-        sv_stop_server();
+        srv.stop();
     end
 
 endmodule
